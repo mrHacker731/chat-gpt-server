86fb6e72d199de9b9fd1ef0394d0e22e1512cdb2
mrHacker731 <mrhacker11@duck.com>
2023-03-12 21:33:25 +0530

    first commit

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
   console.log("server is running on port 4000");
 });
